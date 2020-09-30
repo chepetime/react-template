53f7e3507d28a81f478672f9480cee3c488ea879
José Lugo <chepe.time@gmail.com>
2020-09-30 12:23:38 -0500

    Added Tailwind components

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Page from "components/Page/Page";
 
 export default function About() {
   return (
-    <div>
+    <Page>
       <h1>About</h1>
-    </div>
+    </Page>
   );
 }
